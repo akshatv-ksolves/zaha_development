d57e6b02b3fa50988424d14e8457c145f8e15feb
prashanth poojari <prashantha.poojari@ksolves.com>
2024-08-09 14:23:55 +0530

    [FIX] Fix folders

--- bi_tour_management/models/agent_commision_line.py
+++ bi_tour_management/models/agent_commision_line.py
@@ -17,3 +17,5 @@ class agent_commision_line(models.Model):
     company_id = fields.Many2one('res.company', string='Company', required=True, default=lambda self: self.env.company)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
+# test comment
